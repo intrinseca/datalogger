5268baa3b69d18f91e63c40fd08f0df79bca0752
James Buckley <jebo90@cam.ac.uk>
2012-06-03 14:22:37 +0100

    embedded: ISRs and timer working

--- firmware/main.c
+++ firmware/main.c
@@ -1,25 +1,14 @@
 #include "usb.h"
 #include "USB/usb_function_generic.h"
 #include "user.h"
+#include "timer.h"
+#include "isr.h"
 
 extern void _startup (void);        // See c018i.c in your C18 compiler dir
 #pragma code _RESET_INTERRUPT_VECTOR = 0x000800
 void _reset (void)
 {
     _asm goto _startup _endasm
-}
-#pragma code
-
-#pragma code _HIGH_INTERRUPT_VECTOR = 0x000808
-void _high_ISR (void)
-{
-    ;
-}
-
-#pragma code _LOW_INTERRUPT_VECTOR = 0x000818
-void _low_ISR (void)
-{
-    ;
 }
 #pragma code
 
@@ -37,16 +26,18 @@ void main()
 
     while(1)
     {
-        USBDeviceTasks();
-        ProcessIO();
+//        USBDeviceTasks();
+//        ProcessIO();
     }
 }
 
 static void InitializeSystem()
 {
-    ADCON1 |= 0x0F;
-    UserInit();
-    USBDeviceInit();
+    isr_init();
+    timer_init();
+    //ADCON1 |= 0x0F;
+    //UserInit();
+    //USBDeviceInit();
 }
 
 void USBCBInitEP(void)
